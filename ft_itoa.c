4029815d91674d417e0b7fd12fd3f9ea8c45896d
Elisabete Vaz <evaz@c1r20s7.42lisboa.com>
2021-12-13 20:35:44 +0000

    ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evaz <evaz@student.42lisboa.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 20:54:32 by evaz              #+#    #+#             */
+/*   Updated: 2021/12/06 20:54:32 by evaz             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void	*tmp;
+
+	tmp = malloc(size);
+	if (!tmp)
+		return (NULL);
+	ft_bzero(tmp, size);
+	return (tmp);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*number;
+
+	number = (char *)ft_memalloc(10);
+	if (n >= 0 && number)
+	{
+		*--number = '0' + (n % 10);
+		n /= 10;
+		while (n != 0)
+		{
+			*--number = '0' + (n % 10);
+			n /= 10;
+		}
+	}
+	else if (number)
+	{
+		*--number = '0' - (n % 10);
+		n /= 10;
+		while (n != 0)
+		{
+			*--number = '0' - (n % 10);
+			n /= 10;
+		}
+		*--number = '-';
+	}
+	return (ft_strdup(number));
+}
